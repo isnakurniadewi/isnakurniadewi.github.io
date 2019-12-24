f92038cfd14b734e09fc4f5ee13607c855d481ef
isnakurniadewi <54732045+isnakurniadewi@users.noreply.github.com>
2019-12-25 04:02:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,94 +1,60 @@
-const CACHE_NAME = "pwa2";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/detail.html",
-  "/pages/beranda.html",
-  "/pages/matches.html",
-  "/pages/saved.html",
-  "/pages/standing.html",
-  "/css/materialize.min.css",
-  "/js/materialize.min.js",
-  "/js/api.js",
-  "/js/nav.js",
-  "/js/db.js",
-  "/js/idb.js",
-  "/js/push.js",
-  "/manifest.json",
-  "/icon.png"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+if(workbox){
+  console.log( `Workbox berhasil di load`);
+} else {
+  console.log(`Workbox gagal dimuat`);
+}
 
-self.addEventListener("fetch", function(event) {
-  var base_url = "https://api.football-data.org/v2/";
-  event.respondWith(
-    caches
-      .match(event.request, { cacheName: CACHE_NAME })
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
+workbox.precaching.precacheAndRoute([
+  {url: '/', revision:'1'},
+  {url: '/index.html', revision:'1'},
+  {url: '/detail.html', revision:'1'},
+  {url: '/nav.html', revision:'1'},
+  {url: '/pages/beranda.html', revision:'1'},
+  {url: '/pages/matches.html', revision:'1'},
+  {url: '/pages/saved.html', revision:'1'},
+  {url: '/pages/standing.html', revision:'1'},
+  {url: '/css/materialize.min.css', revision:'1'},
+  {url: '/js/materialize.min.js', revision:'1'},
+  {url: '/js/api.js', revision:'1'},
+  {url: '/idb.js', revision:'1'},
+  {url: '/db.js', revision:'1'},
+  {url: '/nav.js', revision:'1'},
+  {url: '/push.js', revision:'1'},
+  {url: '/manifest.json', revision:'1'},
+  {url: '/icon.png', revision:'1'},
+]);
 
-        var fetchRequest = event.request.clone();
+workbox.routing.registerRoute(
+  new RegExp('/'),
+  workbox.strategies.staleWhileRevalidate()
+);
 
-        return fetch(fetchRequest).then(function(response) {
-          if (!response || response.status !== 200) {
-            return response;
-          }
+workbox.routing.registerRoute(
+  new RegExp('https://api.football-data.org/v2/'),
+  workbox.strategies.staleWhileRevalidate()
+);
 
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-
-          return response;
-        });
-      })
-  );
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener("push", function(event) {
+self.addEventListener("push", function(event){
   var body;
-  if (event.data) {
-    body = event.data.text();
-  } else {
-    body = "Push message no payload";
+  if(event.data){
+      body = event.data.text();
+  }else{
+      body = "Push message no payload";
   }
 
   var options = {
-    body: body,
-    icon: "icon.png",
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    }
+      body: body,
+      icon: '/icon.png',
+      vibrate: [100,50,100],
+      data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+      }
   };
 
   event.waitUntil(
-    self.registration.showNotification("Push Notification", options)
+      self.registration.showNotification('Push Notification', options)
   );
 });
